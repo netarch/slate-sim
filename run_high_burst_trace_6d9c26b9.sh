100477249016fb976c54a0b3541a41edd6a5a08a
gangmuk <gangmuk@gmail.com>
2023-03-15 16:17:34 -0500

    shell script that runs one alibaba trace. (This trace was used for HotOS.)

---
+++ run_high_burst_trace_6d9c26b9.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+output_dir="log"
+
+app="three_depth"
+load_balancer="RoundRobin"
+window_size=100
+base_rps=8
+
+experiment="Alibaba_trace"
+workload="6d9c26b9"
+request_arrival_file=${dir}"/request_arrival_time_6d9c26b9_burst_40_high.txt"
+fixed_autoscaler=0
+# routing_algorithm="heuristic_TE"
+# routing_algorithm="MCLB"
+routing_algorithm="LCLB"
+
+dir="/Users/gangmuk2/projects/Microserver_EventDrivenSimulator"
+python3 ${dir}/simulator.py --app ${app} \
+                    --base_rps ${base_rps} \
+                    --experiment ${experiment} \
+                    --request_arrival_file ${request_arrival_file} \
+                    --workload ${workload} \
+                    --load_balancer ${load_balancer} \
+                    --fixed_autoscaler ${fixed_autoscaler} \
+                    --routing_algorithm ${routing_algorithm} \
+                    --output_dir ${output_dir}
