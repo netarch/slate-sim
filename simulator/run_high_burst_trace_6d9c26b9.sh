3c1fe81a86dc13fe855ee848c75e5d45ed1870f8
gangmuk <gangmuk@gmail.com>
2023-05-26 22:14:10 -0500

    Relocate files (run shell, workload_generator)

--- simulator/run_high_burst_trace_6d9c26b9.sh
+++ simulator/run_high_burst_trace_6d9c26b9.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 output_dir="log"
-
 app="three_depth"
 load_balancer="RoundRobin"
 window_size=100
 base_rps=8
-
 experiment="Alibaba_trace"
 workload="6d9c26b9"
-request_arrival_file=${dir}"/request_arrival_time_6d9c26b9_burst_40_high.txt"
+# request_arrival_file="request_arrival_time_6d9c26b9_burst_40_high.txt"
+request_arrival_file="../parser/request_arrival_time_6d9c26b9.txt"
 fixed_autoscaler=0
+
 # routing_algorithm="heuristic_TE"
 # routing_algorithm="MCLB"
 routing_algorithm="LCLB"
 
-dir="/Users/gangmuk2/projects/Microserver_EventDrivenSimulator"
-python3 ${dir}/simulator.py --app ${app} \
+python3 simulator.py --app ${app} \
                     --base_rps ${base_rps} \
                     --experiment ${experiment} \
                     --request_arrival_file ${request_arrival_file} \
