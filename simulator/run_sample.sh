af8039975dfa05781574d38cb8f3c65b5776c523
gangmuk <gangmuk@gmail.com>
2023-05-28 23:33:28 -0500

    Add sample request arrival time files

--- simulator/run_sample.sh
+++ simulator/run_sample.sh
@@ -3,27 +3,24 @@
 output_dir="log"
 app="three_depth"
 load_balancer="RoundRobin"
-window_size=100
-base_rps=8
-experiment="Alibaba_trace"
-workload="sample"
-request_arrival_file="request_arrival-sample.txt"
+workload="sample2"
 fixed_autoscaler=0
+c0_request_arrival_file="sample-request_arrival-cluster0.txt"
+c1_request_arrival_file="sample-request_arrival-cluster1.txt"
 
-# routing_algorithm="heuristic_TE"
-# routing_algorithm="MCLB"
-# routing_algorithm="LCLB"
-
-# for routing_algorithm in "LCLB"
-for routing_algorithm in "LCLB" "MCLB" "heuristic_TE"
+# for routing_algorithm in "LCLB" "MCLB" "heuristic_TE"
+for routing_algorithm in "LCLB"
 do
+    start=`date +%s`
     python3 simulator.py --app ${app} \
-                        --base_rps ${base_rps} \
-                        --experiment ${experiment} \
-                        --request_arrival_file ${request_arrival_file} \
+                        --c0_request_arrival_file ${c0_request_arrival_file} \
+                        --c1_request_arrival_file ${c1_request_arrival_file} \
                         --workload ${workload} \
                         --load_balancer ${load_balancer} \
                         --fixed_autoscaler ${fixed_autoscaler} \
                         --routing_algorithm ${routing_algorithm} \
                         --output_dir ${output_dir}
+    end=`date +%s`
+    runtime=$((end-start))
+    echo "${routing_algorithm}: ${runtime}s"
 done
