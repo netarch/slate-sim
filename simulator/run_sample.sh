9aee013db07fc212a572d6cb92eec041e049011d
gangmuk <gangmuk@gmail.com>
2023-05-29 13:24:50 -0500

    Update run_sample.sh

--- simulator/run_sample.sh
+++ simulator/run_sample.sh
@@ -9,18 +9,46 @@ c0_request_arrival_file="sample-request_arrival-cluster0.txt"
 c1_request_arrival_file="sample-request_arrival-cluster1.txt"
 
 # for routing_algorithm in "LCLB" "MCLB" "heuristic_TE"
-for routing_algorithm in "LCLB"
-do
-    start=`date +%s`
-    python3 simulator.py --app ${app} \
-                        --c0_request_arrival_file ${c0_request_arrival_file} \
-                        --c1_request_arrival_file ${c1_request_arrival_file} \
-                        --workload ${workload} \
-                        --load_balancer ${load_balancer} \
-                        --fixed_autoscaler ${fixed_autoscaler} \
-                        --routing_algorithm ${routing_algorithm} \
-                        --output_dir ${output_dir}
-    end=`date +%s`
-    runtime=$((end-start))
-    echo "${routing_algorithm}: ${runtime}s"
-done
+# do
+#     start=`date +%s`
+#     python3 simulator.py --app ${app} \
+#                         --c0_request_arrival_file ${c0_request_arrival_file} \
+#                         --c1_request_arrival_file ${c1_request_arrival_file} \
+#                         --workload ${workload} \
+#                         --load_balancer ${load_balancer} \
+#                         --fixed_autoscaler ${fixed_autoscaler} \
+#                         --routing_algorithm ${routing_algorithm} \
+#                         --output_dir ${output_dir}
+#     end=`date +%s`
+#     runtime=$((end-start))
+#     echo "${routing_algorithm}: ${runtime}s"
+# done
+
+
+# routing_algorithm="LCLB"
+# python3 simulator.py --app ${app} \
+#                     --c0_request_arrival_file ${c0_request_arrival_file} \
+#                     --c1_request_arrival_file ${c1_request_arrival_file} \
+#                     --workload ${workload} \
+#                     --load_balancer ${load_balancer} \
+#                     --fixed_autoscaler ${fixed_autoscaler} \
+#                     --routing_algorithm ${routing_algorithm} \
+#                     --output_dir ${output_dir} &
+routing_algorithm="MCLB"
+python3 simulator.py --app ${app} \
+                    --c0_request_arrival_file ${c0_request_arrival_file} \
+                    --c1_request_arrival_file ${c1_request_arrival_file} \
+                    --workload ${workload} \
+                    --load_balancer ${load_balancer} \
+                    --fixed_autoscaler ${fixed_autoscaler} \
+                    --routing_algorithm ${routing_algorithm} \
+                    --output_dir ${output_dir} &
+routing_algorithm="heuristic_TE"
+python3 simulator.py --app ${app} \
+                    --c0_request_arrival_file ${c0_request_arrival_file} \
+                    --c1_request_arrival_file ${c1_request_arrival_file} \
+                    --workload ${workload} \
+                    --load_balancer ${load_balancer} \
+                    --fixed_autoscaler ${fixed_autoscaler} \
+                    --routing_algorithm ${routing_algorithm} \
+                    --output_dir ${output_dir}
