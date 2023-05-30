74b384e28d4bae85bfa6348f68e15f6b56ae7180
gangmuk <gangmuk@gmail.com>
2023-05-30 10:40:50 -0500

    Add new run shell including hotos reproduction

--- simulator/run_hotos_6d9c26b9.sh
+++ simulator/run_hotos_6d9c26b9.sh
@@ -3,18 +3,20 @@
 output_dir="log"
 app="three_depth"
 load_balancer="RoundRobin"
-workload="sample2-2"
+workload="hotos-6d9c26b9"
 fixed_autoscaler=0
-c0_request_arrival_file="sample-request_arrival-cluster0.txt"
-c1_request_arrival_file="sample-request_arrival-cluster1.txt"
 
+c0_request_arrival_file="c0-req_arr-three_depth-6d9c26b9.txt"
+c1_request_arrival_file="c1-req_arr-three_depth-6d9c26b9.txt"
+
+#for routing_algorithm in "LCLB"
 for routing_algorithm in "LCLB" "MCLB" "heuristic_TE"
 do
     start=`date +%s`
     python3 simulator.py --app ${app} \
+                        --workload ${workload} \
                         --c0_request_arrival_file ${c0_request_arrival_file} \
                         --c1_request_arrival_file ${c1_request_arrival_file} \
-                        --workload ${workload} \
                         --load_balancer ${load_balancer} \
                         --fixed_autoscaler ${fixed_autoscaler} \
                         --routing_algorithm ${routing_algorithm} \
